25b674f2ac37ffc3aa37ce1a78b87e8fd1c564a8
Gui Rambo <insidegui@gmail.com>
2022-12-26 11:38:43 -0300

    Raised deployment target to macOS 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MacPreviewUtils",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "MacPreviewUtils",
